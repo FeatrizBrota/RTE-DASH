5eaeb93b1eec07de2b4da1a19a5cfdc8d25d6c4e
Ana Beatriz Frota <113127483+FeatrizBrota@users.noreply.github.com>
2024-04-30 11:00:30 -0300

    Update connector_db.py

--- backend/connector_db.py
+++ backend/connector_db.py
@@ -1,24 +1,24 @@
 import mysql.connector
 
 conexao_dev = mysql.connector.connect(
-    host='rteop-dtb.econeteditora.com.br',
-    port=3306,
-    user='douglas_rangel',
+    host='',
+    port=,
+    user='',
     password='',
     database='rte')
 
 
 conexao_hml = mysql.connector.connect(
-    host='rteop-dtb.econeteditora.com.br',
-    port=3307,
-    user='douglas_rangel',
+    host='',
+    port=,
+    user='',
     password='',
-    database='rte')
+    database='')
 
 
 conexao_prod = mysql.connector.connect(
-    host='database-tributacao.cluster-cgbav3shbm0x.sa-east-1.rds.amazonaws.com',
-    port=3306,
-    user='douglas_rangel',
+    host='',
+    port=,
+    user='',
     password='',
     database='rte')
